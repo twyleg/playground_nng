71a452940d75219378e65b27d47f6c2227a43fa8
Torsten Wylegala <torsten.wylegala@volkswagen.de>
2023-05-17 10:01:11 +0200

    WIP Qt <-> pynng example

---
+++ pynng/pubsub/publisher2.py
@@ -0,0 +1,54 @@
+import pynng
+import time
+
+from test_image_generator import TestImageGenerator
+
+address = "ipc:///tmp/publisher2.ipc"
+
+
+def send_lap_start(pub: pynng.Pub0):
+
+    payload = '''
+    {
+      "sector_1_best_time": "<TIME>",
+      "sector_2_best_time": "<TIME>",
+      "sector_3_best_time": "<TIME>".
+      "lap_best_time": "<TIME>"
+    } '''
+
+    msg = f"lap_start: {payload}"
+    print(f"Publishing lap_start: {msg}")
+    pub.send(msg.encode())
+
+def send_section_finished(pub: pynng.Pub0):
+    payload = '''
+    {
+      "sector_number": 1/2/3
+      "sector_time": "<TIME>"
+      "sector_valid": "true/false"
+      "type": yellow/green/purple
+    } '''
+
+    msg = f"section_finished: {payload}"
+    print(f"Publishing section_finished: {msg}")
+    pub.send(msg.encode())
+
+
+if __name__ == '__main__':
+
+    test_image_generator = TestImageGenerator()
+
+    with pynng.Pub0() as pub:
+        pub.listen(address)
+
+        i = 0
+        while True:
+
+            send_section_finished(pub)
+
+            time.sleep(1.0)
+
+            # image = test_image_generator.get_test_image()
+            # print(f'Publishing image {i}')
+            # pub.send(image.tobytes())
+            # time.sleep(1.0/60.0)
--- pynng/pubsub/subscriber.py
+++ pynng/pubsub/subscriber.py
@@ -1,19 +1,96 @@
+import select
+
 import pynng
 import cv2
 import numpy as np
+import os
+import sys
 
-address = "ipc:///tmp/pubsub.ipc"
+from pathlib import Path
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QTimer
+
+from stopwatch_model import Model, State
+
+
+address_publisher1 = "ipc:///tmp/publisher1.ipc"
+address_publisher2 = "ipc:///tmp/publisher2.ipc"
+
+
+class Gui:
+
+    def __init__(self) -> None:
+        self.diff = 0
+        self.stopwatch_model = Model(0, 0, 0, 0)
+
+        self.app = QGuiApplication(sys.argv)
+        self.engine = QQmlApplicationEngine()
+
+        self.engine.rootContext().setContextProperty("stopwatch_model", self.stopwatch_model)
+        self.engine.load(os.fspath(Path(__file__).resolve().parent / "../frontend/qml/main.qml"))
+
+    def run(self) -> None:
+        if not self.engine.rootObjects():
+            sys.exit(-1)
+        sys.exit(self.app.exec())
 
 
 if __name__ == "__main__":
-    with pynng.Sub0() as sock:
-        sock.subscribe("")
-        sock.dial(address)
 
-        while True:
-            msg = sock.recv_msg()
+    with pynng.Sub0() as subscriber1,\
+            pynng.Sub0() as subscriber2:
+        subscriber1.subscribe("lap_start")
+        subscriber1.dial(address_publisher1)
 
-            image = np.frombuffer(msg.bytes, np.uint8).reshape(720, 1280, 3)
+        subscriber2.subscribe("section_finished")
+        subscriber2.dial(address_publisher2)
 
-            cv2.imshow('image', image)
-            key = cv2.waitKey(1)
+        inputs = [subscriber1.recv_fd, subscriber2.recv_fd]
+
+        sock_fd_dict = {
+            subscriber1.recv_fd: subscriber1,
+            subscriber2.recv_fd: subscriber2
+        }
+
+    gui = Gui()
+    gui.run()
+
+
+
+    # with pynng.Sub0() as subscriber1,\
+    #         pynng.Sub0() as subscriber2:
+    #     subscriber1.subscribe("lap_start")
+    #     subscriber1.dial(address_publisher1)
+    #
+    #     subscriber2.subscribe("section_finished")
+    #     subscriber2.dial(address_publisher2)
+    #
+    #     inputs = [subscriber1.recv_fd, subscriber2.recv_fd]
+    #
+    #     sock_fd_dict = {
+    #         subscriber1.recv_fd: subscriber1,
+    #         subscriber2.recv_fd: subscriber2
+    #     }
+    #
+    #     while True:
+    #
+    #         readable_fds, _, _ = select.select(inputs, [], [])
+    #
+    #         for readable_fd in readable_fds:
+    #             subscriber = sock_fd_dict[readable_fd]
+    #             msg = subscriber.recv_msg()
+    #             print(str(msg.bytes))
+
+
+            # msg1 = sock1.recv_msg()
+            # print(str(msg1.bytes))
+            #
+            # msg2 = sock2.recv_msg()
+            # print(str(msg2.bytes))
+
+            # image = np.frombuffer(msg.bytes, np.uint8).reshape(720, 1280, 3)
+            #
+            # cv2.imshow('image', image)
+            # key = cv2.waitKey(1)
